b2ffa06aaed241558595e67b1fbff699a094f2b2
Kondo Takeru <takechihorn@gmail.com>
2021-01-09 00:43:59 +0900

    login,signup

---
+++ pages/signup.vue
@@ -0,0 +1,117 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-dialog v-model="dialog" persistent max-width="500px" min-width="360px">
+        <div>
+          <v-tabs
+            v-model="tab"
+            show-arrows
+            background-color="blue accent-4"
+            icons-and-text
+            dark
+            grow
+          >
+            <v-tabs-slider color="purple darken-4"></v-tabs-slider>
+            <v-tab v-for="i in tabs" :key="i">
+              <v-icon large>{{ i.icon }}</v-icon>
+              <div class="caption py-1">{{ i.name }}</div>
+            </v-tab>
+            <v-tab-item>
+              <v-card class="px-4">
+                <v-card-text>
+                  <v-form ref="loginForm" v-model="valid" lazy-validation>
+                    <v-row>
+                      <v-col cols="12">
+                        <v-text-field
+                          v-model="loginUsername"
+                          :rules="loginUsernameRules"
+                          label="Username"
+                          required
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12">
+                        <v-text-field
+                          v-model="loginPassword"
+                          :append-icon="show1 ? 'eye' : 'eye-off'"
+                          :rules="[rules.required, rules.min]"
+                          :type="show1 ? 'text' : 'password'"
+                          name="input-10-1"
+                          label="Password"
+                          hint="At least 8 characters"
+                          counter
+                          @click:append="show1 = !show1"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col class="d-flex" cols="12" sm="6" xsm="12"> </v-col>
+                      <v-spacer></v-spacer>
+                      <v-col class="d-flex" cols="12" sm="3" xsm="12" align-end>
+                        <v-btn
+                          x-large
+                          block
+                          :disabled="!valid"
+                          color="success"
+                          @click="validate"
+                        >
+                          Signup
+                        </v-btn>
+                      </v-col>
+                    </v-row>
+                  </v-form>
+                </v-card-text>
+              </v-card>
+            </v-tab-item>
+          </v-tabs>
+        </div>
+      </v-dialog>
+    </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    dialog: true,
+    tab: 0,
+    tabs: [{ name: 'Signup', icon: 'mdi-account-plus' }],
+    valid: true,
+
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    verify: '',
+    loginPassword: '',
+    loginUsername: '',
+    loginUsernameRules: [
+      (v) => !!v || 'Required',
+      (v) => (v && v.length >= 8) || 'Min 8 characters',
+    ],
+
+    show1: false,
+    rules: {
+      required: (value) => !!value || 'Required.',
+      min: (v) => (v && v.length >= 8) || 'Min 8 characters',
+    },
+  }),
+  computed: {
+    passwordMatch() {
+      return () => this.password === this.verify || 'Password must match'
+    },
+  },
+  methods: {
+    validate() {
+      if (this.$refs.loginForm.validate()) {
+        // submit form to server/API here...
+      }
+    },
+    reset() {
+      this.$refs.form.reset()
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation()
+    },
+  },
+}
+</script>
+
+<style></style>
